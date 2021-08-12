b9835b951f2261ec9681464ecc90a7f76da46f91
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-12 20:58:16 +0530

    update : add delete, archive, label(add+delete)

--- controllers/pinned.notes.controller.js
+++ controllers/pinned.notes.controller.js
@@ -1,5 +1,7 @@
+const { ArchiveNote } = require("../models/archive.notes.model");
 const { Note } = require("../models/notes.model");
 const { PinnedNote } = require("../models/pinned.notes.model");
+const { Trash } = require("../models/trash.model");
 
 exports.get_pinned_notes = async(req,res)=>{
     try {
@@ -45,18 +47,13 @@ exports.add_pinnedNote_to_note = async(req,res)=>{
       notes:[filterNote[0]]
     })
       await AddNewNote.save()
-      await PinnedNote.updateOne({"_id":decodedValues.userId},
-            {"$pull":
-              {"pinnedNotes":
-                  {"_id":req.body.noteId}
-              }})
-      return res.json({success:true,message:"pinnednote added to notes array"})
+    }else{
+      await Note.updateOne({"_id":decodedValues.userId},{
+        "$addToSet":{
+          "notes":filterNote[0]
+        }
+      })
     }
-    await Note.updateOne({"_id":decodedValues.userId},{
-      "$addToSet":{
-        "notes":filterNote[0]
-      }
-    })
     await PinnedNote.updateOne({"_id":decodedValues.userId},
     {"$pull":
       {"pinnedNotes":
@@ -66,5 +63,95 @@ exports.add_pinnedNote_to_note = async(req,res)=>{
 
   } catch (error) {
       res.status(500).json({errorMessage:"pinned note not added to notes[]"})
+  }
+}
+
+exports.delete_note_from_pinnedNotes = async(req,res)=>{
+  try {
+    const {decodedValues} = req.user;
+    const getNotesOfUser = await PinnedNote.findById(decodedValues.userId)
+   const filterNote = getNotesOfUser.pinnedNotes.filter((note)=>note._id == req.body.noteId)
+   const checkPinnedNote = await Trash.findById(decodedValues.userId)
+   if(checkPinnedNote === null){
+    const AddNewNote = new Trash({
+      _id : decodedValues.userId,
+      trashNotes:[filterNote[0]]
+    })
+      await AddNewNote.save()
+    }else{
+      await Trash.updateOne({"_id":decodedValues.userId},{
+        "$addToSet":{
+          "trashNotes":filterNote[0]
+        }
+      })
+    }
+    await PinnedNote.updateOne({"_id":decodedValues.userId},
+    {"$pull":
+      {"pinnedNotes":
+          {"_id":req.body.noteId}
+      }})
+    return res.json({success:true,message:"note deleted successfully from pinnedNote"})
+
+  } catch (error) {
+      res.status(500).json({errorMessage:"error while deleting note from pinnedNote"})
+  }
+}
+
+exports.archive_pinned_note = async(req,res)=>{
+  try {
+    const {decodedValues} = req.user;
+    const getNotesOfUser = await PinnedNote.findById(decodedValues.userId)
+   const filterNote = getNotesOfUser.pinnedNotes.filter((note)=>note._id == req.body.noteId)
+   const checkPinnedNote = await ArchiveNote.findById(decodedValues.userId)
+   if(checkPinnedNote === null){
+    const AddNewNote = new ArchiveNote({
+      _id : decodedValues.userId,
+      archiveNotes:[filterNote[0]]
+    })
+      await AddNewNote.save()
+    }else{
+      await ArchiveNote.updateOne({"_id":decodedValues.userId},{
+        "$addToSet":{
+          "archiveNotes":filterNote[0]
+        }
+      })
+    }
+    await PinnedNote.updateOne({"_id":decodedValues.userId},
+    {"$pull":
+      {"pinnedNotes":
+          {"_id":req.body.noteId}
+      }})
+    return res.json({success:true,message:"pinned note archived sucessfully"})
+
+  } catch (error) {
+      res.status(500).json({errorMessage:"error while archiving the pinned note"})
+  }
+}
+
+exports.add_label_to_pinned_notes = async(req,res)=>{
+  try {
+    const {decodedValues} = req.user;
+    await PinnedNote.updateOne({ "_id": decodedValues.userId, "pinnedNotes._id": req.body.noteId }, {
+      "$addToSet": {
+        "pinnedNotes.$.label": req.body, 
+      }
+    })
+      res.json({message:"Label added sucessfully to pinned notes"})
+  } catch (error) {
+      res.status(500).json({errorMessage:"error occured while adding the label to the pinned note"})
+  }
+}
+
+exports.delete_label_from_pinned_note = async(req,res)=>{
+  try {
+    const {decodedValues} = req.user;
+    await PinnedNote.updateOne({ "_id": decodedValues.userId, "pinnedNotes._id": req.body.noteId }, {
+      "$pull": {
+        "pinnedNotes.$.label": {"_id":req.body.labelId}, 
+      }
+    }, { upsert: true })
+      res.json({message:"Label removed sucessfully from pinned notes"})
+  } catch (error) {
+      res.status(500).json({errorMessage:"error occured while removing the label from the pinned note"})
   }
 }
