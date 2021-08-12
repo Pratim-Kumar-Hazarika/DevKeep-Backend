8547a9103e05f3d29375f081e98e831d2701172d
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-12 20:57:27 +0530

    create :label controller

---
+++ controllers/label.controller.js
@@ -0,0 +1,62 @@
+const { Label } = require("../models/label.model")
+
+exports.get_all_labels = async(req,res)=>{
+    try {
+        const {decodedValues} = req.user
+        const getLables = await Label.findById(decodedValues.userId)
+        res.json({message:"All the lables are",getLables})
+    } catch (error) {
+        res.status(500).json({errorMessage:"Error while getting the labels of the user"})
+    }
+}
+
+exports.add_new_label = async(req,res)=>{
+    try {
+        const {decodedValues} = req.user;
+        const getUserLabels = await Label.findById(decodedValues.userId)
+        if(getUserLabels === null){
+            const AddNewLabel = new Label({
+              _id : decodedValues.userId,
+              labels:[req.body]
+            })
+            await AddNewLabel.save()
+            return res.json({success:true,message:"Label added sucessfully"})
+          }
+          await Label.updateOne({"_id":decodedValues.userId},{
+            "$addToSet":{
+              "labels":req.body
+            }
+          })
+          return res.json({success:true,message:"Label Added Successfully"})
+    } catch (error) {
+        res.status(500).json({errorMessage:"Error occured while adding the label"})
+    }
+}
+
+exports.edit_label  = async (req, res) => {
+    try {
+        const {decodedValues} = req.user
+        await Label.updateOne({ "_id": decodedValues.userId, "labels._id": req.body.labelId }, {
+        "$set": {
+          "labels.$.labelName": req.body.newLabel,
+        }
+      }, { upsert: true })
+      res.json({ success: true, message: "Label updated sucessfully" })
+    } catch{
+      res.status(500).json({ success: false, message: "Error while updating the label " })
+    }
+  }
+
+  exports.delete_label = async(req,res)=>{
+      try {
+        const {decodedValues} = req.user
+        await Label.updateOne({"_id":decodedValues.userId},
+        {"$pull":
+          {"labels":
+              {"_id":req.body.labelId}
+          }})
+          res.json({message:"Label deleted successfully"})
+      } catch (error) {
+          res.status(500).json({errorMessage:"Label not deleted"})
+      }
+  }
