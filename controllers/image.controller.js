498e6b7022de447c5682742ea76403186bd9c441
PratimKumarHazarika <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-08-14 00:39:41 +0530

    update : fix req coming from body

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -2,17 +2,17 @@ const { ArchiveNote } = require("../models/archive.notes.model")
 const { Note } = require("../models/notes.model")
 const { PinnedNote } = require("../models/pinned.notes.model")
 const { cloudinary} = require("../cloudinary/cloudinary");
-// uplodedResponse.secure_url
+
 exports.add_image_to_notes = async (req, res) => {
     try {
         const {decodedValues} = req.user
-        // const image = req.body.image;
-        // const uplodedResponse = await cloudinary.uploader.upload(image,{
-        //   upload_preset:"dev_setups"
-        // })
+        const image = req.body.image;
+        const uplodedResponse = await cloudinary.uploader.upload(image,{
+          upload_preset:"dev_setups"
+        })
         await Note.updateOne({ "_id": decodedValues.userId, "notes._id": req.body.noteId }, {
           "$addToSet": {
-            "notes.$.images": {"imageUrl":req.body.imageUrl}, 
+            "notes.$.images": {"imageUrl":uplodedResponse.secure_url}, 
           }
       }, { upsert: true })
       res.json({ success: true, message: "Image uploded sucessfully" })
@@ -24,13 +24,13 @@ exports.add_image_to_notes = async (req, res) => {
   exports.add_image_to_pinned_notes = async (req, res) => {
     try {
         const {decodedValues} = req.user
-           // const image = req.body.image;
-        // const uplodedResponse = await cloudinary.uploader.upload(image,{
-        //   upload_preset:"dev_setups"
-        // })
+          const image = req.body.image;
+        const uplodedResponse = await cloudinary.uploader.upload(image,{
+          upload_preset:"dev_setups"
+        })
         await PinnedNote.updateOne({ "_id": decodedValues.userId, "pinnedNotes._id": req.body.noteId }, {
         "$addToSet": {
-          "pinnedNotes.$.images": {"imageUrl":req.body.imageUrl},
+          "pinnedNotes.$.images": {"imageUrl":uplodedResponse.secure_url},
         }
       }, { upsert: true })
       res.json({ success: true, message: "Image uploded sucessfully to pinned notes" })
@@ -42,13 +42,13 @@ exports.add_image_to_notes = async (req, res) => {
   exports.add_image_to_archive_note = async (req, res) => {
     try {
         const {decodedValues} = req.user;
-         // const image = req.body.image;
-        // const uplodedResponse = await cloudinary.uploader.upload(image,{
-        //   upload_preset:"dev_setups"
-        // })
+         const image = req.body.image;
+        const uplodedResponse = await cloudinary.uploader.upload(image,{
+          upload_preset:"dev_setups"
+        })
         await ArchiveNote.updateOne({ "_id": decodedValues.userId, "archiveNotes._id": req.body.noteId }, {
         "$addToSet": {
-          "archiveNotes.$.images": {"imageUrl":req.body.imageUrl},
+          "archiveNotes.$.images": {"imageUrl":uplodedResponse.secure_url},
         }
       }, { upsert: true })
       res.json({ success: true, message: "Image uplod sucessfully to archive note" })
